ac7e7e6d269d3411c5aa4d9871eb5e48ec9e79b0
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-16 23:10:00 +0300

    Project: Budgety - Reading data from input is now working

--- projects/budgety/app.js
+++ projects/budgety/app.js
@@ -7,16 +7,42 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function(){
 
+    // DOM Strings
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };        
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 
 // MAIN CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function() {
 
-        console.log('fine');
         // 1. Get the field input data
+
+        var input = UIController.getInput();
+        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -27,7 +53,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
        
@@ -35,7 +61,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         if(event.keyCode === 13 || event.which === 13) {
            ctrlAddItem();
         }
+
     })
-
-
+    
 })(budgetController, UIController);
