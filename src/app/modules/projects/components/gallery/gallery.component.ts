57950b5dae2706af8146d1c96def703beccf6a75
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-05-07 23:28:29 +0300

    add hover effect on each figure

--- src/app/modules/projects/components/gallery/gallery.component.ts
+++ src/app/modules/projects/components/gallery/gallery.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  projects: any = [
+    { id: 1, name: 'Cook4You', img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '' , alt: 'photo' },
+    { id: 2, name: 'DateMe', img: 'https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', link: '' , alt: 'photo'},
+    { id: 3, name: 'Weatherspot', img: 'https://images.unsplash.com/photo-1514852451047-f8e1d1cd9b64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1112&q=80', link: '', alt: 'photo' },
+    { id: 4, name: 'Shopanoid', img: 'https://images.unsplash.com/photo-1514997618358-2c490d913b78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', link: '', alt: 'photo' },
+    { id: 5, name: 'Budgety', img: '../../../../../assets/thumbs/857204-min.jpg', link: '' },
+    { id: 6, name: 'Pig Game', img: 'https://images.unsplash.com/photo-1483918793747-5adbf82956c4?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', link: '', alt: 'photo' },
+  ];
 
   constructor() { }
 
