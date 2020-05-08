ac1249ce7c99f0ec1f78185cf7bc8874476bfcf0
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-05-08 09:50:52 +0300

    remove unnecessary things

--- src/app/modules/projects/components/gallery/gallery.component.ts
+++ src/app/modules/projects/components/gallery/gallery.component.ts
@@ -9,6 +9,7 @@ export class GalleryComponent implements OnInit {
   projects: any = [
     { id: 1, name: 'Cook4You', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '' , alt: 'photo' },
     { id: 2, name: 'DateMe', subtext: 'ASP.NET + Angular 9 application', img: 'https://images.unsplash.com/photo-1518104593124-ac2e82a5eb9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', link: '' , alt: 'photo'},
+    // tslint:disable-next-line: max-line-length
     { id: 3, name: 'Weatherspot', subtext: 'javascript application', img: '../../../../../assets/projects/weatherspot-min.jpg', link: '', alt: 'photo' },
     { id: 4, name: 'Shopanoid', subtext: 'wordpress application', img: 'https://images.unsplash.com/photo-1526745925052-dd824d27b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '', alt: 'photo' },
     { id: 5, name: 'Budgety', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '' },
