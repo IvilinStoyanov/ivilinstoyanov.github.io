480b60e52a335f75973256bdda6b68b8d91bfb95
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-05-08 09:22:11 +0300

    updating images to match projects

--- src/app/modules/projects/components/gallery/gallery.component.ts
+++ src/app/modules/projects/components/gallery/gallery.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit {
   projects: any = [
     { id: 1, name: 'Cook4You', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '' , alt: 'photo' },
-    { id: 2, name: 'DateMe', subtext: 'ASP.NET + Angular 9 application', img: 'https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', link: '' , alt: 'photo'},
-    { id: 3, name: 'Weatherspot', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1514852451047-f8e1d1cd9b64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1112&q=80', link: '', alt: 'photo' },
-    { id: 4, name: 'Shopanoid', subtext: 'wordpress application', img: 'https://images.unsplash.com/photo-1514997618358-2c490d913b78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', link: '', alt: 'photo' },
-    { id: 5, name: 'Budgety', subtext: 'javascript application', img: '../../../../../assets/thumbs/857204-min.jpg', link: '' },
-    { id: 6, name: 'Pig Game', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1483918793747-5adbf82956c4?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', link: '', alt: 'photo' },
+    { id: 2, name: 'DateMe', subtext: 'ASP.NET + Angular 9 application', img: 'https://images.unsplash.com/photo-1518104593124-ac2e82a5eb9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', link: '' , alt: 'photo'},
+    { id: 3, name: 'Weatherspot', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1516912481808-3406841bd33c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1883&q=80', link: '', alt: 'photo' },
+    { id: 4, name: 'Shopanoid', subtext: 'wordpress application', img: 'https://images.unsplash.com/photo-1526745925052-dd824d27b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '', alt: 'photo' },
+    { id: 5, name: 'Budgety', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '' },
+    { id: 6, name: 'Pig Game', subtext: 'javascript application', img: 'https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', link: '', alt: 'photo' },
   ];
 
   constructor() { }
