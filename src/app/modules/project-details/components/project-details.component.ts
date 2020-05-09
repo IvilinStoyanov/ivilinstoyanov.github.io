f41c6ca3d37930c8f30f27433ad7f27c65576104
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-05-09 13:33:22 +0300

    add test card structure

--- src/app/modules/project-details/components/project-details.component.ts
+++ src/app/modules/project-details/components/project-details.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.css']
+  styleUrls: ['./project-details.component.scss']
 })
 export class ProjectDetailsComponent implements OnInit {
   id: number;
