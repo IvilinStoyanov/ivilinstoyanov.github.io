df6b7982414eee6e32ee86b3f868383ba3b3926a
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-05-10 23:26:20 +0300

    add languages

--- src/app/modules/project-details/components/project-details.component.ts
+++ src/app/modules/project-details/components/project-details.component.ts
@@ -17,16 +17,29 @@ export class ProjectDetailsComponent implements OnInit {
       img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
       detailsLink: '/projects/details/1',
       projectLink: 'https://cook4you.netlify.com',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2018-11-2'),
+      languages: [
+        { name: 'javascript', link: 'https://www.javascript.com' }
+      ]
     },
     {
       id: 2,
       name: 'DateMe',
-      subtext: 'ASP.NET + Angular 9 application',
+      subtext: 'ASP.NET Core + angular 8 application',
       img: 'https://images.unsplash.com/photo-1518104593124-ac2e82a5eb9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
       detailsLink: '/projects/details/2',
       projectLink: '',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2019-01-20'),
+      languages: [
+        { name: 'c#', link: '' },
+        { name: 'typescript', link: '' },
+        { name: 'html', link: '' },
+        { name: 'css', link: '' },
+
+      ],
+      description: "DateMe is a platform for meeting people and finding true love. People can create and edit their profile, interact with eachother via chat. Users are able to uplaod pictures and create, like and comment on posts. Users also can like eachother's profiles and see who liked theirs."
     },
     {
       id: 3,
@@ -35,7 +48,8 @@ export class ProjectDetailsComponent implements OnInit {
       img: '../../../../../assets/projects/weatherspot-min.jpg',
       detailsLink: '/projects/details/3',
       projectLink: 'https://weatherspot.netlify.com',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2019-02-09')
     },
     {
       id: 4,
@@ -44,7 +58,8 @@ export class ProjectDetailsComponent implements OnInit {
       img: 'https://images.unsplash.com/photo-1526745925052-dd824d27b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
       detailsLink: '/projects/details/4',
       projectLink: '',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2019-04-07')
     },
     {
       id: 5,
@@ -53,7 +68,8 @@ export class ProjectDetailsComponent implements OnInit {
       img: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
       detailsLink: '/projects/details/5',
       projectLink: 'https://ivilinstoyanov.github.io/budgety',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2018-10-24')
     },
     {
       id: 6,
@@ -62,7 +78,8 @@ export class ProjectDetailsComponent implements OnInit {
       img: 'https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
       detailsLink: '/projects/details/6',
       projectLink: 'https://ivilinstoyanov.github.io/pig-game',
-      alt: 'photo'
+      alt: 'photo',
+      dateCreated: new Date('2018-10-25')
     },
   ];
 
@@ -76,7 +93,7 @@ export class ProjectDetailsComponent implements OnInit {
   getDetailsForProject(id: number) {
     this.projects.forEach(element => {
       if (id === element.id) {
-      this.projectDetails = element;
+        this.projectDetails = element;
       }
     });
   }
