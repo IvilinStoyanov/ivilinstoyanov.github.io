8800af1b169a35afe086b79455ed2130fb1e6a5f
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-06-02 18:34:57 +0300

    modify skills section to work with ngIf

--- src/app/modules/skills/components/skills.component.ts
+++ src/app/modules/skills/components/skills.component.ts
@@ -8,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
   skills: any = [
     { name: 'html', value: 100 },
-    { name: 'css', value: 100 }
+    { name: 'css', value: 100 },
+    { name: 'less/scss', value: 100 },
+    { name: 'boostrap', value: 100 },
+    { name: 'javascript', value: 100 },
+    { name: 'jQuery', value: 100 },
+    { name: 'Angular', value: 100 },
+    { name: 'c#', value: 100 },
+    { name: 'asp.net', value: 90 },
+    { name: 'git', value: 100 },
+    { name: 'sql', value: 90 },
+    { name: 'wordpress', value: 80 },
+    { name: 'photoshop', value: 90 }
+  ];
+
+  toLearnSkills: any = [
+    { name: 'reactjs', value: 0 },
+    { name: 'react native', value: 0 },
+    { name: 'node.js', value: 0 },
   ];
 
   constructor() { }
