1170b41cca49338f7f5564cd82959c3925e0f6eb
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-11-06 22:53:01 +0200

    update links and design

--- src/app/components/photoshop/photoshop.component.ts
+++ src/app/components/photoshop/photoshop.component.ts
@@ -13,28 +13,32 @@ export class PhotoshopComponent implements OnInit {
   ngOnInit() {
     this.Images = [
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/17/11/37/fantasy-5578844_1280.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694809/photoshop/118412950_310707523529949_512597547460220876_n.jpg_pwzjrg.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/17/11/31/book-5578836_1280.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694802/photoshop/119476217_4462437430495616_3924363412036315651_n.jpg_ua2ltn.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/17/11/39/fantasy-5578855_960_720.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694820/photoshop/117820289_754243572094634_7923414399529040969_n.jpg_uzoue2.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/26/15/01/fantasy-5604313_960_720.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694800/photoshop/120144539_176467690715297_5245875841384352767_n.jpg_gjynj9.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/17/11/44/fantasy-5578867_960_720.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694814/photoshop/118211984_825185931219875_646820410573557730_n.jpg_nmkldm.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/10/24/13/12/fantasy-5681505_960_720.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604695267/photoshop/92348143_528981351325711_1746605853357539189_n.jpg_zcmyl0.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/09/21/18/08/fantasy-5590898_1280.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694799/photoshop/122412270_672372490359256_8156050410271731220_n.jpg_ztk3db.jpg'
       },
       {
-        src: 'https://cdn.pixabay.com/photo/2020/10/03/14/07/android-hologram-5623624_960_720.jpg'
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604694805/photoshop/119066276_763835114470362_824795753472763697_n.jpg_djkchg.jpg'
+      },
+
+      {
+        src: 'https://res.cloudinary.com/portfolio1118/image/upload/v1604695532/photoshop/android-min_wucpcg.jpg'
       }
     ];
   }
